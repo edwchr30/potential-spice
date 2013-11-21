11039603ec32cf1e6616517473d477fc3f639485
edwchr30 <edwchr30@evergreen.edu>
2013-11-21 15:38:41 -0800

    Create main.py
    Created title and prompt for first step.

---
+++ main.py
@@ -0,0 +1,17 @@
+##Title##
+print (This program serves to demonstrate\
+  that encryption offers protection\
+  beyond intrusion prevention.  With\
+  encryption and properly managed\
+  keys, you can protect your data\
+  even if it gets stolen/misplaced.)\
+  \
+  (The steps will be explained as
+  we go.)
+\
+\
+\
+print (First we will create a file.\
+  Please enter some text to be encrypted.)
+
+\
