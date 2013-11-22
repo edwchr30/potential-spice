20ba966185bc12002ee3c013df7323cee4b3e2dc
edwchr30 <edwchr30@evergreen.edu>
2013-11-21 16:01:06 -0800

    Update main.py
    Added input

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 ##Title##
-print (This program serves to demonstrate\
-  that encryption offers protection\
-  beyond intrusion prevention.  With\
-  encryption and properly managed\
-  keys, you can protect your data\
-  even if it gets stolen/misplaced.)\
-  \
-  (The steps will be explained as
-  we go.)
-\
-\
-\
-print (First we will create a file.\
-  Please enter some text to be encrypted.)
-
-\
+print "This program serves to demonstrate"
+print "that encryption offers protection"
+print "beyond intrusion prevention.  With"
+print "encryption and properly managed"
+print "keys, you can protect your data"
+print "even if it gets stolen/misplaced."
+print "\n"
+print "The steps will be explained as"
+print "we go."
+print "\n"
+print "First we will create a file."
+plaintext = raw_input('Please enter 16 characters to be encrypted: ')
+if len(plaintext) != 16:
+    print "Error! Input must be 16 characters long."
