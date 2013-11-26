5607e2bdb2ced45b98a5b1dcaaf23425ddefee65
edwchr30 <christopher.k.edwards@gmail.com>
2013-11-25 18:20:40 -0800

    testing

--- connect.py
+++ connect.py
@@ -1,9 +1,11 @@
+import socket, ssl
+
 HSM = raw_input('Please enter the IP address of the machine you are connecting to.')
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sslSocket = ssl.wrap_socket(sock, 
-	keyfile="./CA/AKMClientPrivateKey.pem", 
-	certfile="./CA/AKMClientSignedCert.pem", 
-	ca_certs="./CA/TCASelfSignedCert.pem", 
+	keyfile="./AKMClientPrivateKey.pem", 
+	certfile="./AKMClientSignedCert.pem", 
+	ca_certs="./TCASelfSignedCert.pem", 
 	cert_reqs=ssl.CERT_REQUIRED)
-sslSocket.connect((HSM, 6000))
+sslSocket.connect((192.168.160.128, 6000))
 print "Connection is successful!"
