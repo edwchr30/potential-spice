416d366f1e518cc34f66683fbfc18578eb7d5fa3
edwchr30 <christopher.k.edwards@gmail.com>
2013-11-26 21:35:00 -0800

    a coupla notes
    A lot of work, a little change.

--- Notes.md
+++ Notes.md
@@ -10,6 +10,14 @@ I should have started earlier, but I wont back date.
 If my memory was perfect, I wouldn't have to write
 this all down.
 
+11/26/2013
+----------------
+Spent the evening diggin' through stack overflow and getting the Hardware Security Module Virtual Machine configured properly.
+
+11/25/2013
+----------------
+Researched and played around trying to get my ssl syntax working properly.
+
 11/22/2013
 ----------------
 Got my input loop functioning properly
--- connect.py
+++ connect.py
@@ -1,11 +1,12 @@
+import os
 import socket, ssl
 
-HSM = raw_input('Please enter the IP address of the machine you are connecting to.')
+# HSM = raw_input('Please enter the IP address of the machine you are connecting to.')
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sslSocket = ssl.wrap_socket(sock, 
 	keyfile="./AKMClientPrivateKey.pem", 
 	certfile="./AKMClientSignedCert.pem", 
 	ca_certs="./TCASelfSignedCert.pem", 
 	cert_reqs=ssl.CERT_REQUIRED)
-sslSocket.connect((192.168.160.128, 6000))
+sslSocket.connect(("192.168.160.128", 6000))
 print "Connection is successful!"
