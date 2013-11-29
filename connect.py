c1976e84d026a233f1e6fb13e5f34527a2d1644e
edwchr30 <christopher.k.edwards@gmail.com>
2013-11-28 22:52:36 -0800

    updated path/filenames to match

--- connect.py
+++ connect.py
@@ -5,9 +5,9 @@ import ssl
 # HSM = raw_input('Please enter the IP address of the machine you are connecting to.')
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sslSocket = ssl.wrap_socket(sock, 
-	keyfile="AKMClientPrivateKey.pem", 
-	certfile="AKMClientSignedCert.pem", 
-	ca_certs="TCASelfSignedCert.pem", 
+	keyfile="./AKMClientPrivKey.pem", 
+	certfile="./AKMClientSignedCert.pem", 
+	ca_certs="./TCASelfSignedCert.pem", 
 	cert_reqs=ssl.CERT_REQUIRED)
 sslSocket.connect(('192.168.160.128', 6000))
 print "Connection is successful!"
