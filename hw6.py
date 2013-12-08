d19f29c29196f61bea87411f0a1b5b37fb04139b
edwchr30 <christopher.k.edwards@gmail.com>
2013-12-08 14:24:50 -0800

    Personal HW6

---
+++ hw6.py
@@ -0,0 +1,85 @@
+# Name: Chris Edwards
+# Evergreen Login: edwchr30
+# Computer Science Foundations
+# Programming as a Way of Life
+# Homework 6
+
+# You may do your work by editing this file, or by typing code at the
+# command line and copying it into the appropriate part of this file when
+# you are done.  When you are done, running this file should compute and
+# print the answers to all the problems.
+
+
+###
+### Problem 3
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 3 solution follows:"
+
+rounded_height = {}
+rounded_height['John'] = 6
+rounded_height['Mike'] = 5
+rounded_height['Nate'] = 6
+rounded_height['Harry'] = 7
+
+###
+### Problem 4
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 4 solution follows:"
+
+
+print rounded_height['Nate']
+rounded_height['Terry'] = 8
+
+
+###
+### Problem 5
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 5 solution follows:"
+
+M = rounded_height['Mike']
+T = rounded_height['Terry']
+J = rounded_height['John']
+print M
+print T
+print J
+
+###
+### Problem 6
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 6 solution follows:"
+
+x = 0
+n = 10
+for i in range(n):
+    print x
+    x = x + 1
+
+###
+### Problem 7
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 7 solution follows:"
+
+def wloop(y):
+    q = 0
+    while(q < y):
+        print "iteration counter ", q
+        q = q + 1
+        
+wloop(10)
+
+###
+### Collaboration
+###
+
+# ... List your collaborators and other sources of help here (websites, books, etc.),
+# ... as a comment (on a line starting with "#").
